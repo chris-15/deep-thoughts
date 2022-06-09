acb05680f7188eb4d622a12dc7f42dd427bc6fc1
Chris <christopher.sarm15@gmail.com>
2022-06-09 14:13:11 -0400

    integrated the client

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
 
+const path = require('path');
+
+const { authMiddleware }  = require('./utils/auth');
+
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -12,6 +16,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
 
 const app = express();
@@ -24,6 +29,16 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
+
+
+  //serve up static assets
+  if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once("open", () => {
     app.listen(PORT, () => {
